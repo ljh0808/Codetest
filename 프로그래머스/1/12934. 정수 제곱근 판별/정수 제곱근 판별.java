943e96c8e69cc5a5ba49f3979eabaa18a00bf2e4
ljh0808 <122912253+ljh0808@users.noreply.github.com>
2024-10-03 23:01:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 7.09 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        while(answer*answer<n){
+            answer++;
+            
+        }if(answer*answer==n) {answer++;
+        answer = answer * answer;
+                              }
+        else{
+            answer=-1;
+        }
+        return answer;
+    }
+}
