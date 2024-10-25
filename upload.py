efb4052c1c93fbf35767400682d6813ef3d39cda
ljh0808 <122912253+ljh0808@users.noreply.github.com>
2024-10-25 17:53:40 +0900

    Update upload.py
    Signed-off-by: ljh0808 <122912253+ljh0808@users.noreply.github.com>

--- upload.py
+++ upload.py
@@ -3,53 +3,52 @@
 import os
 from urllib import parse
 
-HEADER="""#
+HEADER = """#
 # 백준, 프로그래머스 문제 풀이 목록
 """
 
 def main():
-  content = ""
-  content += HEADER
-  
-  directories = []
-  solveds = []
-  
-  for root, dirs, files in os.walk("."):
-    dirs.sort()
-    if root == '.':
-      for dir in ('.git', '.github'):
-        try:
-          dirs.remove(dir)
-        except ValueError:
-          pass
-      continue
-    
-    category = os.path.basename(root)
-    
-    if category == 'images':
-      continue
-      
-    directory = os.path.basename(os.path.dirname(root))
-    
-    if directory == '.':
-      continue
-      
-    if directory not in directories:
-      if directory in ["백준", "프로그래머스"]:
-        content += "## 📚 {}\n".format(directory)
-      else:
-        content += "### 🚀 {}\n".format(directory)
-        content += "| 문제번호 | 링크 |\n"
-        content += "| ----- | ----- |\n"
-      directories.append(directory)
-      
-    for file in files:
-      if category not in solveds:
-        content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
-        solveds.append(category)
+    content = HEADER
+    directories = []
+    solveds = []
+
+    for root, dirs, files in os.walk("."):
+        dirs.sort()
+        if root == '.':
+            for dir in ('.git', '.github'):
+                try:
+                    dirs.remove(dir)
+                except ValueError:
+                    pass
+            continue
         
-  with open("README.md", "w") as fd:
-    fd.write(content)
+        category = os.path.basename(root)
+
+        if category == 'images':
+            continue
+        
+        directory = os.path.basename(os.path.dirname(root))
+        
+        if directory == '.' or directory == '':
+            continue
+        
+        if directory not in directories:
+            if directory in ["백준", "프로그래머스"]:
+                content += "## 📚 {}\n".format(directory)
+            else:
+                content += "### 🚀 {}\n".format(directory)
+                content += "| 문제번호 | 링크 |\n"
+                content += "| ----- | ----- |\n"
+            directories.append(directory)
+
+        for file in files:
+            relative_path = os.path.relpath(os.path.join(root, file), start=os.getcwd())
+            if category not in solveds:
+                content += "|{}|[링크]({})|\n".format(category, parse.quote(relative_path))
+                solveds.append(category)
+
+    with open("README.md", "w") as fd:
+        fd.write(content)
     
 if __name__ == "__main__":
-  main()
+    main()
